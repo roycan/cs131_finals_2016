9f2f07ec29da6cc3bf7c2f37e52790cbec8fdf79
roycan <roycan@users.noreply.github.com>
2016-12-13 08:17:12 +0800

    additional files for modular and extendable code

--- Michael John Bayuga (Newton + Sys2ODEsRK2)/Sys2ODEsRK2.java
+++ Michael John Bayuga (Newton + Sys2ODEsRK2)/Sys2ODEsRK2.java
@@ -4,16 +4,17 @@ import java.util.*;
 import java.io.*;  
 import static java.lang.System.*;
 import java.util.Scanner; 
-public class Sys2ODEsRK2
+public class Sys2ODEsRK2 extends InputRK2
 {
 
 	 public static void main(String args[]) throws Exception 
 	 {
+	 	InputRK2 irk = new InputRK2();
 	 	ScriptEngineManager mgr = new ScriptEngineManager();
     	ScriptEngine engine = mgr.getEngineByName("JavaScript");
    
 
-   		FileInputStream in = new FileInputStream("ODE1.txt");
+   	/*	FileInputStream in = new FileInputStream("ODE1.txt");
    		FileInputStream on = new FileInputStream("ODE2.txt");
    	 	BufferedReader buf = new BufferedReader(new InputStreamReader(in)); 
    	 	BufferedReader buf2 = new BufferedReader(new InputStreamReader(on)); 	
@@ -25,7 +26,7 @@ public class Sys2ODEsRK2
 	 	Double[] z = new Double[256]; 
 	 	Double N;
 	 	Scanner scan = new Scanner(System.in);
-	 	System.out.println("First value of x");
+	 	System.out.println("First value of x(or most cases t)");
 	 	a = scan.nextDouble();
 	 	System.out.println("First value of y");
 	 	yINI = scan.nextDouble();
@@ -36,32 +37,34 @@ public class Sys2ODEsRK2
 	 	System.out.println("Size of increment");
 	 	h = scan.nextDouble();
 	 	System.out.println(" ");
-	 	System.out.println(" ");
-	 	x[0] = a;
-	 	y[0] = yINI;
-	 	z[0] = zINI;
-	 	N=(b-a)/h;
-	 	for(int i=0;i<N;i++)
+	 	System.out.println(" ");*/
+	 	irk.getin();
+	 	irk.x[0] = irk.a;
+	 	irk.y[0] = irk.yINI;
+	 	irk.z[0] = irk.zINI;
+	 	irk.N=(irk.b-irk.a)/irk.h;
+	 	for(int i=0;i<irk.N;i++)
 	 	{
-	 		x[i+1]= x[i] + h;
-	 		engine.put("y",y[i]);
-			engine.put("z",z[i]);
-	 		Ky1 = 	(Double)engine.eval(line);
-	 		Kz1 = 	(Double)engine.eval(line2);
-	 		engine.put("y",(y[i]+(Ky1*h)));
-			engine.put("z",(z[i]+(Kz1*h)));
-	 		Ky2 = 	(Double)engine.eval(line);
-	 		Kz2 = 	(Double)engine.eval(line2);
-	 		y[i+1]= y[i]+((Ky1+Ky2)*h)/2;
-	  		z[i+1]= z[i]+((Kz1+Kz2)*h)/2;	
+	 		irk.x[i+1]= irk.x[i] + irk.h;
+	 		engine.put("y",irk.y[i]);
+			engine.put("z",irk.z[i]);
+	 		irk.Ky1 = 	(Double)engine.eval(irk.line);
+	 		irk.Kz1 = 	(Double)engine.eval(irk.line2);
+	 		//System.out.println(Ky1);
+	 		engine.put("y",(irk.y[i]+(irk.Ky1*irk.h)));
+			engine.put("z",(irk.z[i]+(irk.Kz1*irk.h)));
+	 		irk.Ky2 = 	(Double)engine.eval(irk.line);
+	 		irk.Kz2 = 	(Double)engine.eval(irk.line2);
+	 		irk.y[i+1]= irk.y[i]+((irk.Ky1+irk.Ky2)*irk.h)/2;
+	  		irk.z[i+1]= irk.z[i]+((irk.Kz1+irk.Kz2)*irk.h)/2;	
 			
 
 	  		System.out.print("x= ");
-			System.out.println(x[i]);
+			System.out.println(irk.x[i]);
 			System.out.print("y= ");
-	 		System.out.println(y[i]);
+	 		System.out.println(irk.y[i]);
 	 		System.out.print("z= ");
-	 		System.out.println(z[i]);
+	 		System.out.println(irk.z[i]);
 	 		System.out.println(" ");
 	 		System.out.println(" ");
 	 	}
