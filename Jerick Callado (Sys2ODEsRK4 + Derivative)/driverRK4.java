43e7d5d2cb80b7322e180ec477bfb101dcb0ab4c
Patricia Caparoso <tricia.caparoso@gmail.com>
2016-12-16 13:35:23 +0800

    Moved input file to another function and made get function for x, y and t

--- Jerick Callado (Sys2ODEsRK4 + Derivative)/driverRK4.java
+++ Jerick Callado (Sys2ODEsRK4 + Derivative)/driverRK4.java
@@ -1,6 +1,4 @@
-import java.io.*;
-import java.util.Scanner;
-import java.io.File;
+
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -8,38 +6,48 @@ import java.io.IOException;
 -This is used to operate the Sys2ODEsRK4.java file
 -place your function in ODE1.java in f method
 -place your function in ODE2.java in f method
--Sys2ODEsRK4.f accepts int n, double h, double array x, double array y and double array t
+-Sys2ODEsRK4.f accepts int n, Double h, Double array x, Double array y and Double array t
 */
 
 public class driverRK4{
-	public static void main(String args[]){
-		Scanner scan = new Scanner(System.in);
+    private static Double a, b, h, xinit, yinit;
+    private static Double[] x, y, t;
+        public void init(Double a, Double b, Double h, Double x, Double y){
+            driverRK4.a = a;
+            driverRK4.b = b;
+            driverRK4.h = h;
+            driverRK4.xinit = x;
+            driverRK4.yinit = y;
+            main();
+        }
+        
+	public static void main(){
+		//Scanner scan = new Scanner(System.in);
 		
-
-		double a, b, h;
-		System.out.print("Input first value of t: ");
+		/*System.out.print("Input first value of t: ");
 		a = Double.parseDouble(scan.nextLine());
 		System.out.print("Input last value of t: ");
 		b = Double.parseDouble(scan.nextLine());
 		System.out.print("Input size of increment of t: ");
 		h = Double.parseDouble(scan.nextLine());
-		double N = (b-a)/h;
+		*/
+                double N = (b-a)/h;
 		int n = (int) N;
 		
-		double x[] = new double[n+2];
-		double y[] = new double[n+2];
-		double t[] = new double[n+2];
+		x = new Double[n+2];
+		y = new Double[n+2];
+		t = new Double[n+2];
 		
 		t[1] = a;
 		t[n] = b;
 		
-		System.out.print("Input initial value of x: ");
-		x[1] = Double.parseDouble(scan.nextLine());
-		System.out.print("Input initial value of y: ");
-		y[1] = Double.parseDouble(scan.nextLine());
+		//System.out.print("Input initial value of x: ");
+		x[1] = xinit;
+		//System.out.print("Input initial value of y: ");
+		y[1] = yinit;
 		
 		Sys2ODEsRK4.f(n, h, x, y, t);
-		FileWriter pp = null;
+	FileWriter pp = null;
 	try{
 		pp = new FileWriter("Sys2ODEsRK4.csv");
 
@@ -75,5 +83,14 @@ public class driverRK4{
 			e.printStackTrace();
 		}
 	}
-}
+    }
+        public Double[] getX(){
+            return driverRK4.x;
+        }
+        public Double[] getY(){
+            return driverRK4.y;
+        }
+        public Double[] getT(){
+            return driverRK4.t;
+        }
 }
