40f90fb03d51212fd9a8a0b73d19d2224a3b17ba
Kirzten Marquez <kacimarquez@gmail.com>
2017-02-01 22:11:54 +0800

    Revert "Refactor EulerImplicit file"
    This reverts commit 1ea11d0c36d4c91a25bb6a1ef089d4ff30639e47.

--- CS 131 Prob1/FINAL/FINAL_IMPLICIT/EulerImplicit.java
+++ CS 131 Prob1/FINAL/FINAL_IMPLICIT/EulerImplicit.java
@@ -1,52 +1,60 @@
+import java.io.File;
+import org.math.plot.*;
+import javax.swing.*;
+
 /*
-* Copyright (c) 2007 Free Software Foundation, Inc. All
-rights reserved.
+-This code uses jmathplot and jmathio libraries.
+-The jar files are included in the folder 
+-"jar xf <.jarfile>" to extract
 */
 
-import java.io.File;
-import javax.swing.*;
-import org.math.plot.*;
-import rootFind.NewtonRoot;
-
 public class EulerImplicit {
-   public static void main(String[] args) {
-		double myX0 = 0, myXN = 0.5, myStepSize = 0.002, myEulerIters;
-		int myTIdx, myNewtonIters;
-		myEulerIters = (myXN - myX0)/myStepSize;
-		myNewtonIters = 250;
-
-		double[] myN = new double[251]; //dependent variable
-    double[] myT = new double[251]; //independent variable
-
-		myN[1] = 2000; myT[1] = myX0;
-
-    for(myTIdx = 1; myTIdx < myEulerIters ; myTIdx++ ) {
-    	  myT[myTIdx + 1] = myT[myTIdx] + myStepSize;
-
-        Double myFuncComponent1 = -(10 * myN[1] * ( 1-Math.exp(-3*(myT[myTIdx+1]))) * myStepSize) - myN[myTIdx];
-        Double myFuncComponent2 = 0.8 * myStepSize;
-
-        String myFunction = "x + x^(3/2) * " + myFuncComponent2 + " + " + myFuncComponent1;
-        String myFuncDerivative = "1 + x^(1/2) * (3/2) * " + myFuncComponent2;
-
-        myN[myTIdx+1] = NewtonRoot.NewtonRoot(myFunction, myFuncDerivative, myN[myTIdx], 0.001, myNewtonIters);
+   // sample client program
+   public static void main(String[] args) { 
+		double a = 0, b = 0.5, h = 0.002, num, denom, xnew=0, x, N;
+		int i,j, jN; 
+		N = (b - a)/h; // number of iterations for euler
+		jN = 250; // number of iterations for newton
+		
+		double[] n = new double[251];
+	    double[] t = new double[251];
+		double[] numarr = new double[250];
+	    double[] denomarr = new double[250];
+		
+		n[1] = 2000; t[1] = a;
+		// IMPLICIT EULER LOOP
+		for(i = 1; i < N ; i++ ) {
+	    	  t[i + 1] = t[i] + h;
+			  x = n[i];
+                          
+                        Double fun_component1 = -(10 * 2000 * ( 1-Math.exp(-3*(t[i+1]))) * h) - x;
+                        Double fun_component2 = 0.8 * h;
+           
+                        String fun = "x + x^(3/2) * " + fun_component2 + " + " + fun_component1;
+                        String funDer = "1 + x^(1/2) * (3/2) * " + fun_component2;
+                          // NEWTON LOOP
+                        double answer;
+                        answer = NewtonRoot.NewtonRoot(fun, funDer, x, 0.001, jN);
+                        n[i+1] = answer;
+		
 		}
+	
 
-
-		for (myTIdx = 1; myTIdx< myEulerIters+1; myTIdx++) {
-  		System.out.print("n[" + myTIdx + "]\t" + myN[myTIdx] + "\n") 	;
-  		System.out.print("t[" + myTIdx + "]\t" + myT[myTIdx] + "\n") 	;
-  		System.out.print("***************************\n");
+		for (i = 1; i< N+1; i++) {
+		System.out.print("n[" + i + "]\t" + n[i] + "\n") 	;
+		System.out.print("t[" + i + "]\t" + t[i] + "\n") 	;
+		System.out.print("***************************\n");
 		}
-
+		
 		Plot2DPanel panel = new Plot2DPanel();
-		panel.addLinePlot("Line", myT, myN);
-
+		panel.addLinePlot("Line", t, n);
+		
 		JFrame frame= new JFrame("Graph n vs t");
 		frame.setContentPane(panel);
 		frame.setSize(500, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 
-	} // End of for( main )
-} // End of for( EulerImplicit )
+	}
+
+}
