36cd124be39115f97c0a9a522a53e7765adbfdcd
Juan Aceron <OneAceron@Gmail.com>
2017-02-11 11:55:57 +0800

    Refactored code for readability

--- Prob5/Prob5.java
+++ Prob5/Prob5.java
@@ -4,105 +4,93 @@ import java.io.IOException;
 
 public class Prob5
 {
-    public Double length;         // L, the length of the fin
-    public Double T0;             // T(0)
-    public Double TL;             // T(L)
-    public Double surroundTemp;   // Ts, the temperature of the surrounding air
 
-    public Double heatCoeff;      // hc, the convective heat coefficient
-    public Double perimeter;      // P, the perimeter bounding the cross-sectional area of the fin
-    public Double epsilon;        // epsilon
-    public Double stefanBoltzmann;// sigma SB, the Stefan-Boltzmann constant
-    public Double k;              // k
-    public Double crossArea;      // Ac, the cross-sectional area of the fin
+    private final Double TEMPERATURE_AT_BASE = 473.0;
+    private final Double TEMPERATURE_AT_TIP = 293.0;
+    private final Double SURROUNDING_TEMP = 293.0;
 
-    public String dT;             // dT/dx
-    public String dw;             // dw/dx
+    private final Double HEAT_COEFF = 40.0;
+    private final Double PERIMETER = 0.016;
+    private final Double EPSILON = 0.4;
+    private final Double STEFAN_BOLTZMANN = 5.67 * Math.pow(10, -8);
+    private final Double THERMAL_CONDUCTIVITY = 240.0;
+    private final Double CROSS_AREA = 1.6 * Math.pow(10, -5);
 
-    public void setProblemParameters()
+    private String myDerivativeT;             // dt/dx
+    private String myDerivativeW;             // dw/dx
+
+    private void setProblemParameters()
     {
-        //problem parameters
-        length = 1.0;
-        T0 = 473.0;
-        TL = 293.0;
-        surroundTemp = 293.0;
-
-        //parameters to use in eq. 17
-        heatCoeff = 40.0;
-        perimeter = 0.016;
-        epsilon = 0.4;
-        stefanBoltzmann = 5.67 * Math.pow(10, -8);
-        k = 240.0;
-        crossArea = 1.6 * Math.pow(10, -5);
-
         //eq 17, where y = T and z = w
-        dT = "z";
+        myDerivativeT = "z";
         String convection_term = String.format("(%f * %f)/(%f * %f) * (y - %f)",
-                                                heatCoeff, perimeter, k, crossArea, surroundTemp);
+                                                HEAT_COEFF, PERIMETER, THERMAL_CONDUCTIVITY, CROSS_AREA, SURROUNDING_TEMP);
         String radiation_term = String.format("(%f * %f * %f)/(%f * %f) * ((y ^ 4) - (%f ^ 4))",
-                                                epsilon, stefanBoltzmann, perimeter, k, crossArea, surroundTemp);
-        dw = convection_term + "+" + radiation_term;
+                                                EPSILON, STEFAN_BOLTZMANN, PERIMETER, THERMAL_CONDUCTIVITY, CROSS_AREA, SURROUNDING_TEMP);
+        myDerivativeW = convection_term + "+" + radiation_term;
     }
 
-    public void calculate(Double guess1, Double guess2)
+    public void calculateTemperature(Double firstGuess, Double secondGuess)
     {
-      Double lowerBound = guess1;
-      Double upperBound = guess2;
+        Double lowerBound = firstGuess;
+        Double upperBound = secondGuess;
 
-      Double[][] lowerResult = Sys2ODEsRK2.calculate(dT, dw, T0, lowerBound, 0.0, 0.1, 0.01);
-      Double[] lowerT = lowerResult[1];
-      Double lowerTL = lowerT[lowerT.length - 1];
+        Double[][] lowerResult = Sys2ODEsRK2.calculate(myDerivativeT, myDerivativeW, TEMPERATURE_AT_BASE, lowerBound);
+        Double[] lowerResult_topRow = lowerResult[1];
+        Double lowerResult_topLeft = lowerResult_topRow[lowerResult_topRow.length - 1];
 
-      Double[][] upperResult = Sys2ODEsRK2.calculate(dT, dw, T0, upperBound, 0.0, 0.1, 0.01);
-      Double[] upperT = upperResult[1];
-      Double upperTL = upperT[upperT.length - 1];
+        Double[][] upperResult = Sys2ODEsRK2.calculate(myDerivativeT, myDerivativeW, TEMPERATURE_AT_BASE, upperBound);
+        Double[] upperResult_topRow = upperResult[1];
+        Double upperResult_topLeft = upperResult_topRow[upperResult_topRow.length - 1];
 
-      Double slope = (upperTL - lowerTL)/(upperBound - lowerBound);
+        Double slope = (upperResult_topLeft - lowerResult_topLeft)/(upperBound - lowerBound);
 
-      System.out.println(lowerTL);
-      System.out.println(upperTL);
+        System.out.println(lowerResult_topLeft);
+        System.out.println(upperResult_topLeft);
 
-      /* I assume that the line is linear, so I draw a line between the two points
-       * Since secant tries to converge to 0 however, I have to subtract TL so that it will converge there instead. */
-      Sec ng = new Sec(String.format("f(x) = %f * (x - %f) + %f - %f", slope, lowerBound, lowerTL, TL),
-                        String.format("Xa = %f", lowerBound),
-                        String.format("Xb = %f", upperBound),
-                        String.format("err = %f", 0.000001),
-                        String.format("imax = %d", 100000)
-                        );
-      Double newGuess = ng.Answer();
-      Double[][] newResult = Sys2ODEsRK2.calculate(dT, dw, T0, newGuess, 0.0, 0.1, 0.01);
-      Double[] newT = newResult[1];
-      Double newTL = newT[newT.length - 1];
+        /* I assume that the line is linear, so I draw a line between the two points
+        * Since secant tries to converge to 0 however, I have to subtract TEMPERATURE_AT_TIP so that it will converge there instead. */
+        Sec secMethod = new Sec(String.format("f(x) = %f * (x - %f) + %f - %f", slope, lowerBound, lowerResult_topLeft, TEMPERATURE_AT_TIP),
+                                String.format("Xa = %f", lowerBound),
+                                String.format("Xb = %f", upperBound),
+                                String.format("err = %f", 0.000001),
+                                String.format("imax = %d", 100000)
+                                );
 
-      Double[] xValues = newResult[0];
+        Double newGuess = secMethod.getRoot();
 
-      String[] titles = {"x", "w(0) = -1000", "w(0) = -3500", String.format("w(0) = %f\n", newGuess)};
-      Double[][] values = {xValues, lowerT, upperT, newT};
-      printCSV("Prob5.csv", titles, values);
+        Double[][] newResult = Sys2ODEsRK2.calculate(myDerivativeT, myDerivativeW, TEMPERATURE_AT_BASE, newGuess);
+        Double[] newResult_topRow = newResult[1];
+        Double newResult_topLeft = newResult_topRow[newResult_topRow.length - 1];
+
+        Double[] xValues = newResult[0];
+        String[] csv_titles = {"x", "w(0) = -1000", "w(0) = -3500", String.format("w(0) = %f\n", newGuess)};
+        Double[][] csv_values = {xValues, lowerResult_topRow, upperResult_topRow, newResult_topRow};
+
+        printCSV("Prob5.csv", csv_titles, csv_values);
     }
 
-    public static void printCSV(String filename, String[] titles, Double[][] values)
+    public static void printCSV(String filename, String[] csv_titles, Double[][] csv_values)
     {
       try
       {
-        PrintWriter outputfile = new PrintWriter(filename, "UTF-8");
-        for (int i = 0; i < titles.length; i++)
+        PrintWriter outputFile = new PrintWriter(filename, "UTF-8");
+        for (int i = 0; i < csv_titles.length; i++)
         {
-          outputfile.print(titles[i]);
-          outputfile.print(",");
+          outputFile.print(csv_titles[i]);
+          outputFile.print(",");
         }
-        outputfile.print("\n");
-        for (int j = 0; j < values[0].length; j++)
+        outputFile.print("\n");
+        for (int j = 0; j < csv_values[0].length; j++)
         {
-          for (int i = 0; i < values.length; i++)
+          for (int i = 0; i < csv_values.length; i++)
           {
-            outputfile.print(values[i][j]);
-            outputfile.print(",");
+            outputFile.print(csv_values[i][j]);
+            outputFile.print(",");
           }
-          outputfile.print("\n");
+          outputFile.print("\n");
         }
-        outputfile.close();
+        outputFile.close();
       }
       catch (IOException e)
       {
@@ -114,6 +102,6 @@ public class Prob5
     {
       Prob5 prob5 = new Prob5();
       prob5.setProblemParameters();
-      prob5.calculate(-1000.0, -3500.0);
+      prob5.calculateTemperature(-1000.0, -3500.0);
     }
 }
