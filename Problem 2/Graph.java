2239780a261ab800f026ffe3dc45f89e6cbc5938
Christian Moraleda <cbmoraleda@up.edu.ph>
2017-02-13 22:33:18 +0800

    Edited Graph.java
    Hi. since di ko parin marun yung "Graph.java", tinry ko nalang siya
    iedit with filename "EditedGraph.java". and since both nga ay di
    nagrarun "even with placing mxparser sa directory ng mga files", di ko
    alam kung ano yung pwedeng mali. Eto yung mga inedit ko, i don't know if
    they are correct.
    
    1. changing "data" and "points" to "mydata" and "mypoints". di ko sure
    kung normal attributes sila.
    2. changing "XData" and "YData" to "theXData" and "theYData". not sure
    kung static variables sila
    3. changing "x0", "x1", "y0", "y1" to "InitialXData", "FinalXData",
    "InitialYData", "FinalYData".
    4. Interchanging line(s) of "private final int WIDTH = 720;..." with
    "public Graph(Double[] xData, Double[] yData) {..."

--- Problem 2/Graph.java
+++ Problem 2/Graph.java
@@ -2,13 +2,6 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Graph extends JFrame {
-    private final int WIDTH = 720;
-    private final int HEIGHT = 500;
-    private Container drawable;
-    private JPanel canvas;
-    double SCALE = 30;   // Size of the graph
-    int OFFSET = 120;
-
     public Graph(Double[] xData, Double[] yData) {
         super("Graph");
         drawable = getContentPane();
@@ -16,37 +9,44 @@ public class Graph extends JFrame {
         drawable.add(canvas);
         setSize(WIDTH, HEIGHT);
     }
-    
-    public class GraphCanvas extends JPanel {
-        private double[] data;
-        private int points;
-        private double[] XData;
-        private double[] YData;
+
+	private final int WIDTH = 720;
+    private final int HEIGHT = 500;
+    private Container drawable;
+    private JPanel canvas;
+    double SCALE = 30;   // Size of the graph
+    int OFFSET = 120;
         
-        public GraphCanvas(Double[] xData, Double[] yData) {
+    public class GraphCanvas extends JPanel {
+        private double[] mydata;
+        private int mypoints;
+        private double[] theXData;
+        private double[] theYData;
+        
+        public GraphCanvas(Double[] thexData, Double[] theyData) {
             super();
-            points = xData.length;
+            mypoints = xData.length;
             
-            XData = new double[points];
-            YData = new double[points];
+			theXData = new double[mypoints];
+            theYData = new double[mypoints];
             
             for(int i = 0; i < points; i++) {
-                XData[i] = xData[i].doubleValue();
-                YData[i] = yData[i].doubleValue();
+                theXData[i] = xData[i].doubleValue();
+                theYData[i] = yData[i].doubleValue();
             }
         }
         
         public void paint(Graphics g) {
             Graphics2D g2 = (Graphics2D) g;
             for(int i = 0; i < points - 1; i++) {
-                int x0 = (int) (XData[i] * SCALE);
-                int x1 = (int) (XData[i + 1] * SCALE);
-                int y0 = (int) (YData[i] * SCALE);
-                int y1 = (int) (YData[i + 1] * SCALE);
-                g2.drawLine(x0, y0+OFFSET, x1, y1+OFFSET);
+                int InitialXData = (int) (theXData[i] * SCALE);
+                int FinalXData = (int) (theXData[i + 1] * SCALE);
+                int InitialYData = (int) (theYData[i] * SCALE);
+                int FinalYData = (int) (theYData[i + 1] * SCALE);
+                g2.drawLine(InitialXData, InitialYData+OFFSET, FinalXData, FinalYData+OFFSET);
                 
                 // Print value
-                //g2.drawString(("" + x0 + ", " + y0), x0 - 20, y0 + 10);                   
+                //g2.drawString(("" + InitialXData + ", " + InitialYData), InitialXData - 20, InitialYData + 10);                   
             }
         }
     }
