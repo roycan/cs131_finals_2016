c31fac942380ffbba8ad021b8784102f4638e6cc
Jerrylee Isaac Geronimo <jerryleegeronimo@yahoo.com>
2017-02-20 22:21:46 +0800

    added directory structure

---
+++ CS 131 Prob1/FINAL/maven_final_implicit/src/main/java/final_implicit/App.java
@@ -0,0 +1,13 @@
+package final_implicit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
