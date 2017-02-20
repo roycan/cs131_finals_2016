9b47754a57ad9a85e78b8f85230679149367ae01
Jerrylee Isaac Geronimo <jerryleegeronimo@yahoo.com>
2017-02-20 21:01:48 +0800

    fixed space for driver emf

--- CS 131 Prob1/FINAL/FINAL_MODIFIED/driverEMF.java
+++ CS 131 Prob1/FINAL/FINAL_MODIFIED/driverEMF.java
@@ -9,40 +9,40 @@ import java.util.Scanner;
 import javax.swing.*;
 
 /*
--This code uses mxparser, jmathplot, and jmathio libraries.
--The jar files are included in the folder
--"jar xf <.jarfile>" to extract
--This is the executable file
-*/
+   -This code uses mxparser, jmathplot, and jmathio libraries.
+   -The jar files are included in the folder
+   -"jar xf <.jarfile>" to extract
+   -This is the executable file
+   */
 
 public class driverEMF{
 
-	public static void main(String args[]){
+    public static void main(String args[]){
 
-		Scanner scan = new Scanner(System.in);
-		Function ode = new Function("ode(t, n) = -0.8*n^(3/2)+10*2000*(1-e^(-3*t))");
+        Scanner scan = new Scanner(System.in);
+        Function ode = new Function("ode(t, n) = -0.8*n^(3/2)+10*2000*(1-e^(-3*t))");
 
-		double firstNum = 0, secondNum = 0.5, stepSize = 0.002, yIni = 2000;
-		double tValues[], nValues[];
+        double firstNum = 0, secondNum = 0.5, stepSize = 0.002, yIni = 2000;
+        double tValues[], nValues[];
 
-		EulerModifiedFunction answer = new EulerModifiedFunction();
-		answer.odeModEuler(ode, firstNum, secondNum, stepSize, yIni);
+        EulerModifiedFunction answer = new EulerModifiedFunction();
+        answer.odeModEuler(ode, firstNum, secondNum, stepSize, yIni);
 
-		tValues = answer.getxValues();
-		nValues = answer.getyValues();
+        tValues = answer.getxValues();
+        nValues = answer.getyValues();
 
-		for(int i=0; i<250; i++){
-			System.out.print("t[" + i + "] = " + tValues[i] + "	n[" + i + "] = " + nValues[i] + "\n");
-		}
+        for(int i=0; i<250; i++){
+            System.out.print("t[" + i + "] = " + tValues[i] + "	n[" + i + "] = " + nValues[i] + "\n");
+        }
 
-		Plot2DPanel panel = new Plot2DPanel();
-		panel.addLinePlot("Line", tValues, nValues);
+        Plot2DPanel panel = new Plot2DPanel();
+        panel.addLinePlot("Line", tValues, nValues);
 
-		JFrame frame= new JFrame("Graph n vs t");
-		frame.setContentPane(panel);
-		frame.setSize(500, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+        JFrame frame= new JFrame("Graph n vs t");
+        frame.setContentPane(panel);
+        frame.setSize(500, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
-	}
+    }
 }
