2e673142d7dfb4a82fce9582d7b460fa37d1e1a2
mccabiles <jiggz.cabiles@gmail.com>
2017-12-10 15:53:00 +0800

    Updated classes to take y0 as a vector
    Since the ode function may not always return a single digit, the modified ODEFunction class now takes a vector y as input, with additional methods for vector operations.
    
    The example in the main function uses a vector [pi/4, 0] as initial conditions.
    The f(t ,y) method of the sample ODE uses the definition:
    input:  [ y, v ]
    output: [ v, -g/l * sin(y) ]

--- Cabiles Codia Padilla/src/com/jcjcjp/cs131mp2/Heun.java
+++ Cabiles Codia Padilla/src/com/jcjcjp/cs131mp2/Heun.java
@@ -10,7 +10,7 @@ package com.jcjcjp.cs131mp2;
 *
 * ARGUMENTS:
 *		t0 = initial t
-*		y0 = initial y at t=t0
+*		y0 = initial y vector at t=t0
 *		h = step size
 *		n = number of iterations
 *		f = ODEFunction object
@@ -22,36 +22,39 @@ package com.jcjcjp.cs131mp2;
 
 public abstract class Heun {
 
-	public static double[][] odeHeun( double t0, double y0, double hsize, int n, ODEFunction function )
+	public static double[][] odeSystem( double t0, double[] y0, double h, int n, ODEFunction function )
 	{
 		double[][] values = new double[n][2];
-		double f;
-		double k0, k1;
-		double new_y;
+		double[] f;
+		double[] k0, k1;
+		double[] new_y;
 		double new_t;
 		double old_t = t0;
-		double old_y = y0;
+		double[] old_y = y0;
 		
 		for ( int i = 0; i < n; i++ )
 		{
 
-			print( "y: " + Double.toString(old_y) );
-			print( "t: " + Double.toString(old_t) );
-			print( "\n" );
+			System.out.printf( "%d t: %.2f y: %.6f \n", i, old_t, old_y[0]);
 			
 		/*
 		 * This part implements the actual algorithm: 
-		 * 		k0 = y0 + ( h * f(t0, y0) )
+		 * 		f = f(t0, y0)
+		 * 		k0 = y0 + ( h * f )
 		 * 		k1 = f( t0 + h, k0 )
-		 *		y = y0  + ( (h/2) * ( f( t0, y0 ) + k1 ) )
+		 *		y = y0  + ( (h/2) * ( f + k1 ) )
 		 */
 			
 			f = function.f( old_t, old_y );
-			k0 = old_y + ( hsize * f );
-			k1 = function.f( old_t + hsize, k0);
+			k0 = ODEFunction.add( old_y, ODEFunction.mult(f, h) );
+			k1 = function.f( old_t + h, k0);
+			new_y = ODEFunction.add(
+						old_y ,
+						ODEFunction.mult( 
+							ODEFunction.add(f, k1),
+							h/2 ));
 			
-			new_y = old_y + ( (hsize / 2) * ( f + k1 ) );
-			new_t = old_t + hsize;
+			new_t = old_t + h;
 			
 			old_y = new_y;
 			old_t = new_t;
@@ -61,8 +64,4 @@ public abstract class Heun {
 		return values;
 	}
 	
-	private static void print( String string )
-	{
-		System.out.println( string );
-	}
 }
--- Cabiles Codia Padilla/src/com/jcjcjp/cs131mp2/Main.java
+++ Cabiles Codia Padilla/src/com/jcjcjp/cs131mp2/Main.java
@@ -8,7 +8,7 @@ import com.jcjcjp.cs131mp2.Euler;
  * This serves as a template or guide for the use of the library.
  * ---------------------------------
  * USAGE: 
- * 1. Define your ODE by implementing the ODEFunction interface.
+ * 1. Define your ODE by extending the ODEFunction class.
  * 2. Override the f( t, y ) method with the function you will use.
  * 3. Instantiate your ODE object.
  * 4. Call the methods with the initial values and the ODE object as arguments.
@@ -19,23 +19,26 @@ import com.jcjcjp.cs131mp2.Euler;
  * 
  * 		ARGUMENTS:
  * 			t = initial value of t
- * 			y = initial value of y
+ * 			y = initial values of y vector
  * 			h = step size
  * 			n = number of iterations
  * 			function = ODE object
  * ==================================
  */
 	
-class myODEFunction implements ODEFunction
+class myODEFunction extends ODEFunction
 {
+	// Define your function here:
 	@Override
-	public double f(double t, double y) 
+	public double[] f(double t, double[] y) 
 	{
+		double[] new_y = new double[2];
 		double g = 32.2;
 		double l = 2;
-		double c = Math.sqrt( g / l );
-		double y0 = Math.PI / 4; 
-		return -y0 * c * Math.sin( c * t );
+		
+		new_y[0] = y[1];
+		new_y[1] = (-g/l) * Math.sin( y[0] );
+		return new_y;
 	}
 }
 
@@ -45,21 +48,28 @@ public class Main
 	
 	public static void main(String[] args) {
 
-	// Set the arguments:
+	// Set the initial conditions:
 		double t0 = 0;
-		double y0 = 1;
-		double h = 0.1;
-		int n = 4;
+		
+		double[] y0 = new double[2]; // Our initial condition is y = [ pi/4, 0 ]
+		y0[0] = Math.PI /4;
+		y0[1] = 0;
+		
+		double h = 0.05;
+		int n = 33;
 		
 	// Instantiate your ODE:
 		myODEFunction function = new myODEFunction();
 	
 	// Call the methods:	
 		System.out.println("Euler: ");
-		Euler.odeEuler(t0, y0, h, n, function);
+		Euler.odeSystem(t0, y0, h, n, function);
 		
 		System.out.println("Heun: ");
-		Heun.odeHeun(t0, y0, h, n, function);
+		Heun.odeSystem(t0, y0, h, n, function);
+		
+		System.out.println("\n RK4: ");
+		RK4.odeSystem(t0, y0, 0.01, 100, function);
 		
 	}
 
